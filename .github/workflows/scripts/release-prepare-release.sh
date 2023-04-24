f69d73230e501952808e0e4f66acedfcc34f6e9a
Dmitry Anoshin <anoshindx@gmail.com>
2023-04-23 23:58:57 -0700

    [chore] Add an automation to skip release if there no changes
    Update the release process to skip releasing the stable set of modules if there are no changes since the last version

--- .github/workflows/scripts/release-prepare-release.sh
+++ .github/workflows/scripts/release-prepare-release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+if [ "${CANDIDATE_STABLE}" == "" ] && [ "${CANDIDATE_BETA}" == "" ]; then
+    echo "One of CANDIDATE_STABLE or CANDIDATE_BETA must be set"
+    exit 1
+fi
+
 make chlog-update VERSION="v${CANDIDATE_STABLE}/v${CANDIDATE_BETA}"
 git config user.name opentelemetrybot
 git config user.email 107717825+opentelemetrybot@users.noreply.github.com
@@ -8,8 +13,12 @@ git checkout -b "${BRANCH}"
 git add --all
 git commit -m "Changelog update ${CANDIDATE_BETA}/${CANDIDATE_STABLE}"
 
-make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_STABLE}" RELEASE_CANDIDATE="${CANDIDATE_STABLE}" MODSET=stable
-make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_BETA}" RELEASE_CANDIDATE="${CANDIDATE_BETA}" MODSET=beta
+if [ "${CANDIDATE_STABLE}" != "" ]; then
+    make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_STABLE}" RELEASE_CANDIDATE="${CANDIDATE_STABLE}" MODSET=stable
+fi
+if [ "${CANDIDATE_BETA}" != "" ]; then
+    make prepare-release GH=none PREVIOUS_VERSION="${CURRENT_BETA}" RELEASE_CANDIDATE="${CANDIDATE_BETA}" MODSET=beta
+fi
 git push origin "${BRANCH}"
 
 gh pr create --title "[chore] Prepare release ${CANDIDATE_BETA}/${CANDIDATE_STABLE}" --body "
